8eefc134425af8bc0e1993c9c69fae8021f014e4
Richard Hua <rich@rkhua.com>
2024-05-17 18:18:46 -0700

    Remove code for old approaches

--- examples/rust-wasi-readfile/src/main.rs
+++ examples/rust-wasi-readfile/src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
 use std::fs;
 
-#[no_mangle]
-fn read_file() {
+fn main() {
     let contents =
         fs::read_to_string("/sandbox/file.txt").expect("Something went wrong reading the file");
     println!("Content:\n{}", contents);
